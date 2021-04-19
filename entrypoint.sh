5b40bb4afb79e21a962d88f390d921196a599d24
Xiaobao.Li <Xiaobao.Li@cn.yokogawa.work>
2021-04-19 16:34:49 +0800

    1

---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SS_CONFIG=${SS_CONFIG:-""}
+SS_MODULE=${SS_MODULE:-"ss-server"}
+KCP_CONFIG=${KCP_CONFIG:-""}
+KCP_MODULE=${KCP_MODULE:-"kcpserver"}
+KCP_FLAG=${KCP_FLAG:-"false"}
+
+while getopts "s:m:k:e:x" OPT; do
+    case $OPT in
+        s)
+            SS_CONFIG=$OPTARG;;
+        m)
+            SS_MODULE=$OPTARG;;
+        k)
+            KCP_CONFIG=$OPTARG;;
+        e)
+            KCP_MODULE=$OPTARG;;
+        x)
+            KCP_FLAG="true";;
+		p)
+		    POLIPO_CONFIG=$OPTARG;;
+    esac
+done
+
+export SS_CONFIG=${SS_CONFIG}
+export SS_MODULE=${SS_MODULE}
+export KCP_CONFIG=${KCP_CONFIG}
+export KCP_MODULE=${KCP_MODULE}
+export KCP_FLAG=${KCP_FLAG}
+export POLIPO_CONFIG=${POLIPO_CONFIG}
+
+exec runsvdir -P /etc/service
